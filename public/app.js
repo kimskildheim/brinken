9923579d9a070b142a9511355844689990a10867
kim skildheim <kimskildheim@gmail.com>
2016-03-10 15:52:49 +0100

    Added a tabel and a person section to my site

--- public/app.js
+++ public/app.js
@@ -4,14 +4,49 @@
 
 	myApp.controller("MainController", function () {
 		var appCtrl = this;
-		this.players = player;
+		this.players = players;
 		appCtrl.title = 'FK Brinken';
 	});
 	
-	var player = {
+	var players = [{
 		name: 'Kim',
 		price: 2.96,
-		description: "Midfielder"
+		description: "Midfielder",
+		history:"5 mål",
+		show: true,
+		isNotPlayingAtBrinken: false,
+		images: [{full: 'kim.jpg'}]
+	},
+	{
+		name: 'Torbjørn',
+		price: 1.50,
+		description: "Defender",
+		history:"2 mål",
+		show: true,
+		isNotPlayingAtBrinken: false,
+		images: [{full: 'torbjorn.jpg'}]
+	},
+	{
+		name: 'Halvard',
+		price: 2.60,
+		description: "Forward",
+		history:"4 mål",
+		show: true,
+		isNotPlayingAtBrinken: false,
+		images: [{full: 'halvard.jpg'}]
 	}
+	];
+
+	myApp.controller('PanelController', function(){
+		this.tab = 1;
+
+		this.selectPanel = function(newValue){
+			this.tab = newValue;
+		};
+
+		this.isSelected = function(tabName){
+			return this.tab === tabName;
+		};
+	});
 	
 })();
